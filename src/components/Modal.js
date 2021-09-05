4a49efa5110bc93674ec02619726251ab3ea3f6a
github username <github email address>
2021-09-05 01:01:08 -0400

    add modal

---
+++ src/components/Modal.js
@@ -0,0 +1,15 @@
+class Modal {
+
+  get modal(){
+    return document.querySelector("#myModal")
+  }
+
+  open = () => {
+    this.modal.style.display= "block"
+  }
+
+  close = () => {
+    this.modal.style.display= "none"
+  }
+
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 console.log("hello world")
 const api= new ApiService("http://localhost:3000")
+const modal = new Modal()
+// const user= *****
 
-api.getUsers().then(console.log)
+// api.getUsers().then(console.log)
+
+let seconds = document.getElementById("countdown").textContent;
+const countDown= setInterval(function() {
+  seconds--;
+  document.getElementById("countdown").textContent = seconds;
+  if (seconds <= 0) clearInterval(countDown);
+}, 1000);
+
+const userLoggedin = () => {
+  document.getElementsByClassName("username")
+}
+
+
+
+
